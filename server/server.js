75e33330a384154ef9bf3a93a4010e4e79687cd7
MohamedElmdary <engm5081@gmail.com>
2020-09-16 14:14:43 +0200

    Allow security for https://res.cloudinary.com/

--- server/server.js
+++ server/server.js
@@ -4,25 +4,33 @@ const compression = require('compression');
 const helmet = require('helmet');
 const cors = require('cors');
 const app = express();
+const { join } = require('path');
 
 // prettier-ignore
 app
+    .use(express.static(join(__dirname, '..', 'build')))
     .use(cors())
     .use(bodyParser.json())
     .use(bodyParser.urlencoded({extended: true}))
     .use(helmet())
-    .use(compression());
+    .use(compression())
 
 // delay for demo
-app.use((req, res, next) => {
-    setTimeout(() => {
-        next();
-    }, 500);
-});
+if (!process.env.PORT) {
+    app.use((_, __, next) => {
+        setTimeout(() => {
+            next();
+        }, 500);
+    });
+}
 
 /* routes */
 app.use('/api/products', require('./routes/products'));
 app.use('/api/categories', require('./routes/categories'));
+
+app.get('*', (_, res) => {
+    res.sendFile(join(__dirname, '..', 'build', 'index.html'));
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
