9fda610f96a0da9297f7e706b92ea11aaf483b89
MohamedElmdary <engm5081@gmail.com>
2020-09-17 14:18:08 +0200

    fixed responsive issue in GallerySlider

--- src/components/GallerySlider/index.js
+++ src/components/GallerySlider/index.js
@@ -1,17 +1,25 @@
 import React, { useState } from 'react';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import './GallerySlider.scss';
 
 function GallerySlider({ gallery }) {
+    const matches = useMediaQuery('(max-width: 500px)');
     const [active, setActive] = useState(0);
 
     const indicators = gallery.map((src, key) => {
-        //prettier-ignore
-        const className = `slider__indicators__item ${key === active ? 'active' : ''}`;
+        const className = [
+            'slider__indicators__item',
+            key === active ? 'active' : '',
+            key + 1 === gallery.length ? 'last-item' : '',
+        ].join(' ');
         const onClick = () => setActive(key);
         const alt = 'preview-' + (key + 1);
+        const style = matches
+            ? { width: `calc(${100 / gallery.length}% - 10px)` }
+            : undefined;
 
         return (
-            <div {...{ className, onClick, key }}>
+            <div {...{ className, style, onClick, key }}>
                 <img {...{ src, alt }} />
             </div>
         );
